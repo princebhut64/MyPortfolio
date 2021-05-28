76aa31bf6c0c1acdb4c86889d704d7ad34f0b11e
prince bhut <84903751+princebhut64@users.noreply.github.com>
2021-05-28 10:06:43 +0530

    message field are changed

--- myapp/models.py
+++ myapp/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name =  models.CharField(max_length = 30)
     email =  models.CharField(max_length=30)
-    message = models.TextField(max_length=600, blank=True)
     subject =  models.CharField(max_length = 50)
-    
+    message = models.TextField()
     def __str__(self):
         return self.name+"-"+self.subject
