3ca5c73b68901c7214d9193bcd1bf136ed093769
prince bhut <84903751+princebhut64@users.noreply.github.com>
2021-05-28 10:08:11 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render,redirect
-from .models import Contact
+from . models import Contact
+from django.conf import settings
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
 
-def single(request):
-	return render(request,'single.html')
-
 def contact(request):
-	if request.method=="POST":
-		Contact.objects.create(
-			name=request.POST['name'],
-			email=request.POST['email'],
-			subject=request.POST['subject'],
-			message=request.POST['message']
-			)
-		return redirect('index')
-	else:
-		return redirect('index')
+	Contact.objects.create(
+		name=request.POST['name'],
+		email=request.POST['email'],
+		subject=request.POST['subject'],
+		message=request.POST['message'],
+		)
+	msg="contact saved succesfully.will reach you very soon "
+	return render(request,'index.html',{'msg':msg})
